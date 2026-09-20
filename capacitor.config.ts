0dd88dc09402d398e12c4adf72b25387c3214605
MaxLC1 <maxlaime617@gmail.com>
2026-09-19 23:12:23 -0400

    Add Capacitor Android support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fashionstore.app',
+  appName: 'FashionStore AR',
+  webDir: 'dist/frontend-angular-ar/browser'
+};
+
+export default config;
